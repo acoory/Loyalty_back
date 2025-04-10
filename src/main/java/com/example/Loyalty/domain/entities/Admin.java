3da55c9252d1b867ff87f3f6d2b55597df6a8774
Cory Anthony <anthony.cory@epitech.eu>
2025-04-10 02:41:10 +0200

    fix : login

--- src/main/java/com/example/Loyalty/domain/entities/Admin.java
+++ src/main/java/com/example/Loyalty/domain/entities/Admin.java
@@ -1,6 +1,7 @@
 package com.example.Loyalty.domain.entities;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -23,6 +24,7 @@ public class Admin extends BaseModel {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "enterprise_id", referencedColumnName = "id")
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Enterprise enterprise;
 
 //    private Long enterpriseId;
